1873363199250ba05d49369b5e999b159018526e
cirosanchez <ciroandressanchezbuitrago@gmail.com>
2024-03-04 08:44:11 -0500

    Added new libraries and MiniMessage Support.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,22 @@ repositories {
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/repositories/snapshots")
     maven("https://oss.sonatype.org/content/repositories/central")
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://jitpack.io")
+    maven("https://repo.flyte.gg/releases")
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.20.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+
+    // Commands
+    implementation("com.github.Revxrsal.Lamp:common:3.1.9")
+    implementation("com.github.Revxrsal.Lamp:bukkit:3.1.9")
+
+    // twilight
+    implementation("gg.flyte:twilight:1.1.7")
+
+
 }
 
 tasks.test {
--- src/main/kotlin/me/cirosanchez/template/Template.kt
+++ src/main/kotlin/me/cirosanchez/template/Template.kt
@@ -1,10 +1,17 @@
 package me.cirosanchez.template
 
+import me.cirosanchez.template.extension.Symbol
+import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.plugin.java.JavaPlugin
 
 
 // TODO: rename class and parent package.
 class Template : JavaPlugin() {
+
+
+    companion object{
+        val mm = MiniMessage.miniMessage();
+    }
 
     override fun onEnable() {
         this.logger.info("Plugin activated!")
